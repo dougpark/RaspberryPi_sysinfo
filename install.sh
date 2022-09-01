8cdaa1f2fe918c14f74371b573a2f7943eb40823
Doug Park <parkdn@gmail.com>
2022-08-31 23:32:13 -0500

    chmod

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 mkdir ~/bin
 cd ~/bin
+rm sysinfo
 wget https://raw.githubusercontent.com/dougpark/RaspberryPi_sysinfo/master/sysinfo
+echo running chmod
+chmod +x sysinfo
 cd 
 
-
+# run this script to download and run the installer
 # wget -O - https://raw.githubusercontent.com/dougpark/RaspberryPi_sysinfo/master/install.sh | bash
