6c873caaf6516eea7d5c5d1c6efa7c1ffccfb93b
Doug Park <parkdn@gmail.com>
2022-09-02 18:43:23 -0500

    install

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+echo sysinfo install started
 mkdir ~/bin >/dev/null 2>&1
 cd ~/bin >/dev/null 2>&1
 rm sysinfo >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/dougpark/RaspberryPi_sysinfo/master/sysinfo 
 chmod +x sysinfo >/dev/null 2>&1
-echo Done. Type sysinfo to run
+echo sysinfo install complete. Type sysinfo to run
 
 # run this script to download and run the installer
 # wget -q -O - https://raw.githubusercontent.com/dougpark/RaspberryPi_sysinfo/master/install.sh | bash
