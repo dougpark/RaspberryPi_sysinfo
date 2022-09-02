afc2fce41e1f2be660fb8e7e37af0e2a8f323242
Doug Park <parkdn@gmail.com>
2022-09-02 18:33:04 -0500

    new remote sysinfo rsysinfo

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 mkdir ~/bin
 cd ~/bin
 rm sysinfo
-wget https://raw.githubusercontent.com/dougpark/RaspberryPi_sysinfo/master/sysinfo 
+wget -q https://raw.githubusercontent.com/dougpark/RaspberryPi_sysinfo/master/sysinfo 
 chmod +x sysinfo
 
 # run this script to download and run the installer
-# wget -O - https://raw.githubusercontent.com/dougpark/RaspberryPi_sysinfo/master/install.sh | bash
+# wget -q -O - https://raw.githubusercontent.com/dougpark/RaspberryPi_sysinfo/master/install.sh | bash
