2e2d9d7391dd04a8782be62bfcae69fe15cb85cc
Doug Park <parkdn@gmail.com>
2022-09-13 17:40:52 -0500

    Installer loop through file list

--- install.sh
+++ install.sh
@@ -2,20 +2,22 @@
 
 echo sysinfo install started
 
+BASEPATH=https://raw.githubusercontent.com/dougpark/RaspberryPi_sysinfo/master/
+FILES=( "sysinfo" "rsysinfo" "Help.md")
+
 # Setup ~/bin directory
 mkdir ~/bin >/dev/null 2>&1
 cd ~/bin >/dev/null 2>&1
 
-# Install sysinfo
-rm sysinfo >/dev/null 2>&1
-wget -q https://raw.githubusercontent.com/dougpark/RaspberryPi_sysinfo/master/sysinfo 
-chmod +x sysinfo >/dev/null 2>&1
-
-# Install rsysinfo
-rm rsysinfo >/dev/null 2>&1
-wget -q https://raw.githubusercontent.com/dougpark/RaspberryPi_sysinfo/master/rsysinfo 
-chmod +x rsysinfo >/dev/null 2>&1
-
+# Install files
+for FILE in "${FILES[@]}"
+do
+   rm $FILE >/dev/null 2>&1
+   wget -q ${BASEPATH}${FILE} 
+   chmod +x $FILE >/dev/null 2>&1
+   echo -n "#"
+done
+echo
 echo sysinfo install complete.
 
 # run this script to download and run the installer
