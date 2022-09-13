85b818d7e2a66852889ac4d0b36cb1e6999c74f7
Doug Park <parkdn@gmail.com>
2022-09-13 16:56:22 -0500

    rsysinfo now runs install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
+
 echo sysinfo install started
+
+# Setup ~/bin directory
 mkdir ~/bin >/dev/null 2>&1
 cd ~/bin >/dev/null 2>&1
 
+# Install sysinfo
 rm sysinfo >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/dougpark/RaspberryPi_sysinfo/master/sysinfo 
 chmod +x sysinfo >/dev/null 2>&1
 
+# Install rsysinfo
 rm rsysinfo >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/dougpark/RaspberryPi_sysinfo/master/rsysinfo 
 chmod +x rsysinfo >/dev/null 2>&1
