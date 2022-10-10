15e95a1a12cce3689cee09a489d382992fec0ea1
Doug Park <parkdn@gmail.com>
2022-10-10 12:41:01 -0500

    remove files on update

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 BASEPATH=https://raw.githubusercontent.com/dougpark/RaspberryPi_sysinfo/master/
-FILES=( "sysinfo" "rsysinfo" "Help.md")
+FILES=( "sysinfo")
+REMOVE=( "Help.md", "rsysinfo")
 
-echo sysinfo install started
+echo sysinfo install/update started
 
 # Setup ~/bin directory
 mkdir ~/bin >/dev/null 2>&1
@@ -17,8 +18,14 @@ do
    chmod +x $FILE >/dev/null 2>&1
    echo -n "#"
 done
+# Remove files
+for FILE in "${REMOVE[@]}"
+do
+   rm $FILE >/dev/null 2>&1
+   echo -n "*"
+done
 echo
-echo sysinfo install complete
+echo sysinfo install/update complete
 
 # run this script to download and run the installer
 # wget -q -O - https://raw.githubusercontent.com/dougpark/RaspberryPi_sysinfo/master/install.sh | bash
