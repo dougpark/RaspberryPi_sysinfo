a7637df71e764ce7b6c02bd23fc81071c4e0fc86
Doug Park <parkdn@gmail.com>
2024-05-01 18:47:20 -0500

    changed to $HOME

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 BASEPATH=https://raw.githubusercontent.com/dougpark/RaspberryPi_sysinfo/master/
 FILES=( "sysinfo" "sysinfo_settings" )
 REMOVE=( "Help.md" "rsysinfo" "arg" "custom_settings")
-VER="v1.1.2"
+VER="v1.1.3"
 
 echo sysinfo install/update started $VER
 
@@ -29,20 +29,20 @@ done
 # add sysinfo_settings
 
 # Path to custom settings file
-sysinfo_settings="~/bin/sysinfo_settings"
+sysinfo_settings="$HOME/bin/sysinfo_settings"
 
 # Path to .bashrc
-bashrc_file="~/.bashrc"
+bashrc_file="$HOME/.bashrc"
 
 # Check if sysinfo_settings line already exists in .bashrc
 if ! grep -qF "source $sysinfo_settings" "$bashrc_file"; then
     # Append the source command to .bashrc
     echo "source $sysinfo_settings" >> "$bashrc_file"
     echo
-    echo "Sysinfo settings sourced to ~/.bashrc"
+    echo "Sysinfo settings sourced to $bashrc_fie"
 else
     echo
-    echo "Sysinfo settings already sourced to ~/.bashrc"
+    echo "Sysinfo settings already sourced to $bashrc_file"
 fi
 
 # make sure to reload to include latest sysinfo settings
